7330ad31e617e080b60cf0d4ff6770d83e5e687f
EL amongus <camiloaroca43@gmail.com>
2026-08-31 10:33:32 -0500

    aoush

--- src/forms/auth_forms.py
+++ src/forms/auth_forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField, TelField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional, ValidationError
 from src.models.ModeloUsuario import ModeloUsuario
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=80)])
+    email = EmailField('Email', validators=[DataRequired(), Email(), Length(max=80)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Iniciar Sesión')
 
 class RegisterForm(FlaskForm):
     nombre = StringField('Nombre', validators=[DataRequired(), Length(max=50)])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=80)])
+    email = EmailField('Email', validators=[DataRequired(), Email(), Length(max=80)])
     direccion = StringField('Direccion', validators=[DataRequired(), Length(max=100)])
-    celular = StringField('Celular', validators=[DataRequired(), Length(max=15)])
-    telefono = StringField('Telefono', validators=[Optional(), Length(max=15)])
+    celular = TelField('Celular', validators=[DataRequired(), Length(max=15)])
+    telefono = TelField('Telefono', validators=[Optional(), Length(max=15)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm = PasswordField('Confirmar Password', validators=[DataRequired(), EqualTo('password', message='Las contraseñas deben coincidir')])
     submit = SubmitField('Crear Cuenta')
